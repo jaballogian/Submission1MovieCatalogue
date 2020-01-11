35b81370c0d16bc4ff55bd4905b502e2eaa569b4
jaballogian <jaballogianrma77@gmail.com>
2020-01-11 16:40:14 +0700

    submission 2 : adding tv shows recycler view in tv shows tab layout

--- app/src/main/java/com/example/submission1moviecatalogue/TVShowsFragment.java
+++ app/src/main/java/com/example/submission1moviecatalogue/TVShowsFragment.java
@@ -1,13 +1,18 @@
 package com.example.submission1moviecatalogue;
 
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class TVShowsFragment extends Fragment {
 
+    private RecyclerView movieRecyclerView;
+    private ArrayList<Movie> movies = new ArrayList<>();
 
     public TVShowsFragment() {
         // Required empty public constructor
@@ -25,7 +32,41 @@ public class TVShowsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tvshows, container, false);
+        View view = (View) inflater.inflate(R.layout.fragment_tvshows, container, false);
+
+        movieRecyclerView = (RecyclerView) view.findViewById(R.id.tvShowsRecylerView);
+        movieRecyclerView.setHasFixedSize(true);
+
+        movies.addAll(getListHeroes());
+        showRecyclerList();
+
+        return view;
+    }
+
+    public ArrayList<Movie> getListHeroes() {
+
+        String[] title = getResources().getStringArray(R.array.tvTitle);
+        String[] description = getResources().getStringArray(R.array.tvDescription);
+        String[] releaseDate = getResources().getStringArray(R.array.tvDate);
+        String[] rating = getResources().getStringArray(R.array.tvRating);
+        TypedArray cover = getResources().obtainTypedArray(R.array.tvCover);
+        ArrayList<Movie> movieArrayList = new ArrayList<>();
+        for (int i = 0; i < title.length; i++) {
+            Movie movie = new Movie();
+            movie.setTitle(title[i]);
+            movie.setDescription(description[i]);
+            movie.setReleaseDate(releaseDate[i]);
+            movie.setRating(rating[i]);
+            movie.setCover(cover.getResourceId(i, -1));
+            movieArrayList.add(movie);
+        }
+        return movieArrayList;
+    }
+
+    private void showRecyclerList(){
+        movieRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        MovieRecyclerViewAdapter movieRecyclerViewAdapter = new MovieRecyclerViewAdapter(movies);
+        movieRecyclerView.setAdapter(movieRecyclerViewAdapter);
     }
 
 }
