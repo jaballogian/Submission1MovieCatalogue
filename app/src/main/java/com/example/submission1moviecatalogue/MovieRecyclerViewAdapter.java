2f2dcc694487284cacbfca8101caec911954c21c
jaballogian <jaballogianrma77@gmail.com>
2020-01-11 14:16:35 +0700

    submission 2 : adding movies recycler view in moview tab layout

---
+++ app/src/main/java/com/example/submission1moviecatalogue/MovieRecyclerViewAdapter.java
@@ -0,0 +1,65 @@
+package com.example.submission1moviecatalogue;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecyclerViewAdapter.ListViewHolder> {
+
+    private ArrayList<Movie> movieList;
+
+    public MovieRecyclerViewAdapter(ArrayList<Movie> list) {
+        this.movieList = list;
+    }
+
+    @NonNull
+    @Override
+    public MovieRecyclerViewAdapter.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_recycleview_fragment_movies, parent, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieRecyclerViewAdapter.ListViewHolder holder, int position) {
+        Movie movie= movieList.get(position);
+//        Glide.with(holder.itemView.getContext())
+//                .load(movie.getCover())
+//                .apply(new RequestOptions().override(55, 55))
+//                .into(holder.movieCoverImageView);
+        holder.movieCoverImageView.setImageResource(movie.getCover());
+        holder.movieTitleTextView.setText(movie.getTitle());
+        holder.movieDateTextView.setText(movie.getReleaseDate());
+        holder.movieRatingTextView.setText(movie.getRating());
+        holder.movieDescriptionTextView.setText(movie.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView movieCoverImageView;
+        TextView movieTitleTextView, movieDateTextView, movieRatingTextView, movieDescriptionTextView;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            movieCoverImageView = itemView.findViewById(R.id.movieCoverImageView);
+            movieTitleTextView = itemView.findViewById(R.id.movieTitleTextView);
+            movieDateTextView = itemView.findViewById(R.id.movieDateTextView);
+            movieRatingTextView = itemView.findViewById(R.id.movieRatingTextView);
+            movieDescriptionTextView = itemView.findViewById(R.id.movieDescriptionTextView);
+        }
+    }
+}
--- app/src/main/java/com/example/submission1moviecatalogue/MoviesFragment.java
+++ app/src/main/java/com/example/submission1moviecatalogue/MoviesFragment.java
@@ -2,13 +2,18 @@
 package com.example.submission1moviecatalogue;
 
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -16,6 +21,8 @@ import android.view.ViewGroup;
  */
 public class MoviesFragment extends Fragment {
 
+    private RecyclerView movieRecyclerView;
+    private ArrayList<Movie> movies = new ArrayList<>();
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -26,7 +33,41 @@ public class MoviesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View view = (View) inflater.inflate(R.layout.fragment_movies, container, false);
+
+        movieRecyclerView = (RecyclerView) view.findViewById(R.id.moviesRecylerView);
+        movieRecyclerView.setHasFixedSize(true);
+
+        movies.addAll(getListHeroes());
+        showRecyclerList();
+
+        return view;
+    }
+
+    public ArrayList<Movie> getListHeroes() {
+
+        String[] title = getResources().getStringArray(R.array.movieTitle);
+        String[] description = getResources().getStringArray(R.array.movieDescription);
+        String[] releaseDate = getResources().getStringArray(R.array.movieDate);
+        String[] rating = getResources().getStringArray(R.array.movieRating);
+        TypedArray cover = getResources().obtainTypedArray(R.array.movieCover);
+        ArrayList<Movie> movieArrayList = new ArrayList<>();
+        for (int i = 0; i < title.length; i++) {
+            Movie movie = new Movie();
+            movie.setTitle(title[i]);
+            movie.setDescription(description[i]);
+            movie.setReleaseDate(releaseDate[i]);
+            movie.setRating(rating[i]);
+            movie.setCover(cover.getResourceId(i, -1));
+            movieArrayList.add(movie);
+        }
+        return movieArrayList;
+    }
+
+    private void showRecyclerList(){
+        movieRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        MovieRecyclerViewAdapter movieRecyclerViewAdapter = new MovieRecyclerViewAdapter(movies);
+        movieRecyclerView.setAdapter(movieRecyclerViewAdapter);
     }
 
 }
