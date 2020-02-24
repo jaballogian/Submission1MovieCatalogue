7c3367b5920b9557b01f0ff4c42c7f0b24427f89
jaballogian <jaballogianrma77@gmail.com>
2020-02-24 22:16:52 +0700

    submission 5 : close notification when notification is clicked

--- app/src/main/java/com/example/submission1moviecatalogue/AlarmReceiver.java
+++ app/src/main/java/com/example/submission1moviecatalogue/AlarmReceiver.java
@@ -6,12 +6,12 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
@@ -66,15 +66,22 @@ public class AlarmReceiver extends BroadcastReceiver {
     private void showAlarmNotification(Context context, String title, String message, int notifId) {
         String CHANNEL_ID = "Channel_1";
         String CHANNEL_NAME = "AlarmManager channel";
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.example.submission1moviecatalogue", "com.example.submission1moviecatalogue.MainActivity"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
         NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_access_time_black_24dp)
+                .setSmallIcon(R.drawable.ic_access_time_white_24dp)
+                .setContentIntent(pendingIntent)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setColor(ContextCompat.getColor(context, android.R.color.transparent))
                 .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
-                .setSound(alarmSound);
+                .setSound(alarmSound)
+                .setAutoCancel(true);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
                     CHANNEL_NAME,
@@ -109,4 +116,6 @@ public class AlarmReceiver extends BroadcastReceiver {
         }
 //        Toast.makeText(context, "Repeating alarm set up", Toast.LENGTH_SHORT).show();
     }
+
+
 }
