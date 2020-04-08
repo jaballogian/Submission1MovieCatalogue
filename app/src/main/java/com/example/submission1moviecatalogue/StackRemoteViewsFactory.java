aef36a1cb6ee9cc3b6dcc16b227bb07f570b1a7a
jaballogian <jaballogianrma77@gmail.com>
2020-04-09 01:29:06 +0700

    fixing load images to stackwidget for android above 22

--- app/src/main/java/com/example/submission1moviecatalogue/StackRemoteViewsFactory.java
+++ app/src/main/java/com/example/submission1moviecatalogue/StackRemoteViewsFactory.java
@@ -5,10 +5,19 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,7 +28,9 @@ import java.util.List;
 
 public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory{
 
-    private final List<Bitmap> mWidgetItems = new ArrayList<>();
+//    private final List<Bitmap> mWidgetItems = new ArrayList<>();
+//    private final List<Uri> mWidgetItems = new ArrayList<>();
+    private final List<String> mWidgetItems = new ArrayList<>();
     private final Context mContext;
     private MovieHelper movieHelper;
     private MovieHelperTV movieHelperTV;
@@ -59,8 +70,24 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
     @Override
     public RemoteViews getViewAt(int position) {
 
+
+
         RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
-        rv.setImageViewBitmap(R.id.imageView, mWidgetItems.get(position));
+//        rv.setImageViewBitmap(R.id.imageView, mWidgetItems.get(position));
+//        rv.setImageViewUri(R.id.imageView, mWidgetItems.get(position));
+
+        try {
+            Bitmap bitmap = Glide.with(mContext)
+                    .asBitmap()
+                    .load(mWidgetItems.get(position))
+                    .submit(512, 512)
+                    .get();
+
+            rv.setImageViewBitmap(R.id.imageView, bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Bundle extras = new Bundle();
         extras.putInt(AppWidget.EXTRA_ITEM, position);
         Intent fillInIntent = new Intent();
@@ -101,13 +128,21 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
 
         for(int i = 0; i < titlesMovie.size(); i++){
 
-            mWidgetItems.add(getBitmapFromURL(titlesMovie.get(i)));
+//            mWidgetItems.add(getBitmapFromURL(titlesMovie.get(i)));
+//            convertURLtoBitmap(titlesMovie.get(i), mWidgetItems);
+//            mWidgetItems.add(convertStringToUri(titlesMovie.get(i)));
+            mWidgetItems.add(titlesMovie.get(i));
         }
 
         for(int j = 0; j < titlesTV.size(); j++){
 
-            mWidgetItems.add(getBitmapFromURL(titlesTV.get(j)));
+//            mWidgetItems.add(getBitmapFromURL(titlesTV.get(j)));
+//            convertURLtoBitmap(titlesTV.get(j), mWidgetItems);
+//            mWidgetItems.add(convertStringToUri(titlesTV.get(j)));
+            mWidgetItems.add(titlesTV.get(j));
         }
+
+
     }
 
     public static Bitmap getBitmapFromURL(String src) {
@@ -117,12 +152,34 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
             connection.setDoInput(true);
             connection.connect();
             InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Bitmap myBitmap = BitmapFactory.decodeResourceStream(null, null, input, null, null);
             return myBitmap;
         } catch (IOException e) {
             // Log exception
             return null;
         }
+    }
+
+    private void convertURLtoBitmap(String input, final List<Bitmap> list){
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(input)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        list.add(resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                    }
+                });
+    }
+
+    private Uri convertStringToUri(String input){
+
+        return Uri.parse(input);
     }
 
 }
